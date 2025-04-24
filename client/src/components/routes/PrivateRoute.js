c8e6e821396a585e2fe2397bc10064ae1c510e8f
Vanshsuri07 <vanshsuri07@gmai.com>
2025-04-24 19:20:13 +0530

    Updated Error

--- client/src/components/routes/PrivateRoute.js
+++ client/src/components/routes/PrivateRoute.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { showLoading, hideLoading } from "../../redux/features/alertSlice";
-import { setUser } from "../../redux/features/auth/authSlice";
 import axios from "axios";
-import { Navigate } from "react-router-dom";
+import { setUser } from "../../redux/features/auth/authSlice";
+
+import { useNavigate, Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const getUser = async () => {
     try {
       dispatch(showLoading());
@@ -21,27 +22,31 @@ const PrivateRoute = ({ children }) => {
           },
         }
       );
+
       dispatch(hideLoading());
-      dispatch(setUser(data.data)); // assuming your backend returns user under `data.data`
+      if (data.success) {
+        dispatch(setUser(data.data));
+      } else {
+        localStorage.clear();
+        navigate("/login");
+      }
     } catch (error) {
-      dispatch(hideLoading());
       localStorage.clear();
+      dispatch(hideLoading());
+      console.log(error);
     }
   };
-
   useEffect(() => {
     if (!user) {
       getUser();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  });
 
-  // No token = not logged in
-  if (!localStorage.getItem("token")) {
+  if (localStorage.getItem("token")) {
+    return children;
+  } else {
     return <Navigate to="/login" />;
   }
-
-  return children;
 };
 
 export default PrivateRoute;
